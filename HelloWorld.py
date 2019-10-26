93959f8e0307478dc1866b0d9b0508561b38fd05
Keishirou <nietononosyana.tarutaru@gmail.com>
2019-10-26 15:30:18 +0900

    入力内容の判定を追加

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,10 @@
 print("何か入力してEnterキーを押してください")
 input_line = input()
 print("HelloWorld 2019/10/26")
-print("あなたが入力したのは、" + str(input_line) + "です。")
+print("あなたが入力したのは、" + str(input_line) + "で")
+if str.isalpha(input_line):
+    print("すべて文字ですね。")
+elif str.isdecimal(input_line):
+    print("すべて数字ですね。")
+else:
+    print("英数字混合、または特殊文字を含んでいますね。")
